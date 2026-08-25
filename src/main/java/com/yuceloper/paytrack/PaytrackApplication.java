ba655c33c07b1c839b04d191c608893c5e8ac610
yuceloper <155106626+yuceloper@users.noreply.github.com>
2026-08-25 18:44:29 +0300

    feat: add Spring Boot application entrypoint

---
+++ src/main/java/com/yuceloper/paytrack/PaytrackApplication.java
@@ -0,0 +1,11 @@
+package com.yuceloper.paytrack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaytrackApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(PaytrackApplication.class, args);
+    }
+}
