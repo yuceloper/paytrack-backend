480723620bbf065c4e36890b09ddb8bee5a945d3
yuceloper <155106626+yuceloper@users.noreply.github.com>
2026-08-25 23:57:09 +0300

    feat: add account transaction repository port

---
+++ src/main/java/com/yuceloper/paytrack/account/application/AccountTransactionRepository.java
@@ -0,0 +1,14 @@
+package com.yuceloper.paytrack.account.application;
+
+import com.yuceloper.paytrack.account.domain.AccountTransaction;
+import com.yuceloper.paytrack.account.domain.AccountTransactionType;
+
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Optional;
+
+public interface AccountTransactionRepository {
+    AccountTransaction save(AccountTransaction transaction);
+    List<AccountTransaction> findBetween(Long userId, LocalDate from, LocalDate to);
+    Optional<AccountTransaction> findActiveSourceTransaction(String sourceType, Long sourceId, AccountTransactionType type);
+}
