cc4873842f3c606bbac206ce8018d1c689ef5c7c
yuceloper <155106626+yuceloper@users.noreply.github.com>
2026-08-25 21:44:01 +0300

    refactor: add resource not found helper constructor

--- src/main/java/com/yuceloper/paytrack/shared/exception/ResourceNotFoundException.java
+++ src/main/java/com/yuceloper/paytrack/shared/exception/ResourceNotFoundException.java
@@ -1,7 +1,12 @@
 package com.yuceloper.paytrack.shared.exception;
 
 public class ResourceNotFoundException extends RuntimeException {
+
     public ResourceNotFoundException(String message) {
         super(message);
+    }
+
+    public ResourceNotFoundException(String resourceName, Object id) {
+        super(resourceName + " not found with id: " + id);
     }
 }
