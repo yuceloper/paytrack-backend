940fc40e22646bbf51acadd1aa54cf4d6f99f24b
yuceloper <155106626+yuceloper@users.noreply.github.com>
2026-08-25 21:38:46 +0300

    feat: add user domain module

---
+++ src/main/java/com/yuceloper/paytrack/user/domain/User.java
@@ -0,0 +1,30 @@
+package com.yuceloper.paytrack.user.domain;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "users", uniqueConstraints = @UniqueConstraint(name = "uk_users_email", columnNames = "email"))
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String email;
+
+    private Integer salaryDay;
+
+    @Column(nullable = false)
+    @Builder.Default
+    private boolean active = true;
+}
