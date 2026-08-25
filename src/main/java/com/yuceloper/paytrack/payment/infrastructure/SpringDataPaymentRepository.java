6e0298e2ac5e85da1d1a7d13c925223fa57e6860
yuceloper <155106626+yuceloper@users.noreply.github.com>
2026-08-25 23:21:59 +0300

    feat: support recurrence duplicate check

--- src/main/java/com/yuceloper/paytrack/payment/infrastructure/SpringDataPaymentRepository.java
+++ src/main/java/com/yuceloper/paytrack/payment/infrastructure/SpringDataPaymentRepository.java
@@ -9,4 +9,5 @@ import java.util.List;
 interface SpringDataPaymentRepository extends JpaRepository<Payment, Long> {
     List<Payment> findAllByUserIdAndDueDateBetweenOrderByDueDateAsc(Long userId, LocalDate start, LocalDate end);
     List<Payment> findAllByUserIdAndPaidFalseAndDueDateBeforeOrderByDueDateAsc(Long userId, LocalDate today);
+    boolean existsByUserIdAndNameAndDueDate(Long userId, String name, LocalDate dueDate);
 }
